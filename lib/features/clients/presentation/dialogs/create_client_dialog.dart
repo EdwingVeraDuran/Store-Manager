7b75a4e96f2dd58e4a12b0ddb5effad29e8cf1d0
EdwingVeraDuran <edwingveraduran@gmail.com>
2025-01-29 12:39:17 -0500

    Remove unused import

--- lib/features/clients/presentation/dialogs/create_client_dialog.dart
+++ lib/features/clients/presentation/dialogs/create_client_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:elevarm_ui/elevarm_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:store_manager/features/clients/domain/entities/client.dart';
