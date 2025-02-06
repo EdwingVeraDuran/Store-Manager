5d409898e83e6be42d6231118b0f09c79743095b
EdwingVeraDuran <edwingveraduran@gmail.com>
2025-02-06 13:11:50 -0500

    Update supabase_auth_repo.dart
    removed users table from supabase, updated to user metadata on authentication.

--- lib/features/auth/data/supabase_auth_repo.dart
+++ lib/features/auth/data/supabase_auth_repo.dart
@@ -4,7 +4,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseAuthRepo implements AuthRepo {
   final supabaseAuth = Supabase.instance.client.auth;
-  final supabaseClient = Supabase.instance.client;
 
   @override
   Future<AppUser?> loginWithEmailPassword(String email, String password) async {
@@ -13,16 +12,10 @@ class SupabaseAuthRepo implements AuthRepo {
       final AuthResponse authResponse = await supabaseAuth.signInWithPassword(
           email: email, password: password);
 
-      final clientResponse = await supabaseClient
-          .from("users")
-          .select("*")
-          .eq("id", authResponse.user!.id)
-          .single();
-
       AppUser user = AppUser(
         uid: authResponse.user!.id,
         email: authResponse.user!.email!,
-        name: clientResponse["name"],
+        name: authResponse.user!.userMetadata!["username"],
       );
       return user;
     } catch (e) {
@@ -35,16 +28,12 @@ class SupabaseAuthRepo implements AuthRepo {
       String name, String email, String password) async {
     try {
       // Attempt SignUp
-      final AuthResponse authResponse =
-          await supabaseAuth.signUp(email: email, password: password);
-      final clientResponse = await supabaseClient.from("users").insert({
-        "id": authResponse.user!.id,
-        "name": name,
-      }).select("*");
+      final AuthResponse authResponse = await supabaseAuth
+          .signUp(email: email, password: password, data: {"username": name});
       AppUser user = AppUser(
         uid: authResponse.user!.id,
         email: authResponse.user!.email!,
-        name: clientResponse[0]["name"],
+        name: authResponse.user!.userMetadata!["username"],
       );
       return user;
     } catch (e) {
@@ -65,16 +54,10 @@ class SupabaseAuthRepo implements AuthRepo {
       return null;
     }
 
-    final clientResponse = await supabaseClient
-        .from("users")
-        .select("*")
-        .eq("id", supabaseUser.id)
-        .single();
-
     return AppUser(
       uid: supabaseUser.id,
       email: supabaseUser.email!,
-      name: clientResponse["name"],
+      name: supabaseUser.userMetadata!["username"],
     );
   }
 }
