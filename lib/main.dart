fb38f23e05f60795b041344b99b601fcd73323e0
EdwingVeraDuran <edwingveraduran@gmail.com>
2025-02-06 13:12:27 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_manager/app.dart';
-import 'package:store_manager/utilities/supabase_service.dart';
+import 'package:store_manager/services/supabase_service.dart';
 
 void main() async {
   await SupabaseService.initializeApp();
